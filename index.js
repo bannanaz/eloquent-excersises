0c0ca8e23f721baa7af4d5e2997c06752b6ab8fa
bannanaz <anlepet@gmail.com>
2021-06-21 13:59:12 +0200

    chapter 3

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ for (let counter = 0; counter < 10; counter++) {
 }
 console.log(result);
 
-//prompt loop
+/*prompt loop
 let yourName;
 do {
   yourName = prompt("Who are you?");
 } while (!yourName);
-console.log(yourName);
+console.log(yourName);*/
 
 for (let number = 0; number <= 12; number += 2) {
   console.log(number);
@@ -57,7 +57,7 @@ switch (prompt("What is the weather like?")) {
     break;
 }
 
-//excersises
+//excersises chap 2
 function triangle() {
   for (let line = "#"; line.length < 8; line += "#") {
     console.log(line);
@@ -89,3 +89,85 @@ for (let y = 0; y < size; y++) {
   board += "\n";
 }
 console.log(board);
+
+//chapter 3, functions
+
+//nested scope
+let factor = 2;
+const hummus = function (factor) {
+  const ingredient = function (amount, unit, name) {
+    let ingredientAmount = amount * factor;
+    if (ingredientAmount > 1) {
+      unit += "s";
+    }
+    console.log(`${ingredientAmount} ${unit} ${name}`);
+  };
+  ingredient(1, "can", "chickpeas");
+  ingredient(0.25, "cup", "tahini");
+  ingredient(0.25, "cup", "lemon juice");
+  ingredient(1, "clove", "garlic");
+  ingredient(2, "tablespoon", "olive oil");
+  ingredient(0.5, "teaspoon", "cumin");
+};
+
+/*functions as values
+let launchMissiles = function () {
+  missileSystem.launch("now");
+};
+if (safeMode) {
+  launchMissiles = function () {
+    do nothing
+  };
+}*/
+
+//declaration (order)
+console.log("The future says:", future());
+
+function future() {
+  return "You'll never have flying cars";
+}
+//arrow functions
+const power = (base, exponent) => {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+};
+
+//excersises chap 3
+
+function min(x, y) {
+  if (x < y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+function isEven(n) {
+  if (n == 0) return true;
+  else if (n == 1) return false;
+  else if (n < 0) return isEven(-n);
+  else return isEven(n - 2);
+}
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+
+function countChar(string, ch) {
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
